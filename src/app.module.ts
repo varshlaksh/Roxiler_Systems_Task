3e33b9db70e8a65f2167db82a255bdce9e8fac4a
varshlaksh <varshneylakshya642@gmail.com>
2026-06-19 22:09:31 +0530

    typeorm connection, config module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,41 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { StoresModule } from './stores/stores.module';
 import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, StoresModule, RatingsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+
+    AuthModule,
+    UsersModule,
+    StoresModule,
+    RatingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
